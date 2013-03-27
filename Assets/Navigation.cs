bec458c50ce3221bc36bfcfb9a5c3ac7dc4ad2d9
Andrew Hale <ajh110@case.edu>
2013-03-27 13:13:20 -0400

    Testing navigation

--- Assets/Navigation.cs
+++ Assets/Navigation.cs
@@ -14,6 +14,7 @@ public class Navigation : MonoBehaviour {
 	public float y_rate;
 	public float z_rate;
 	public float time_rate;
+	
 	// Use this for initialization
 	void Start () {
 		 InvokeRepeating("Travel", 1, time_rate);
@@ -25,31 +26,37 @@ public class Navigation : MonoBehaviour {
 	}
 	
 	void Travel (){
+		//store the current position into these three values, update them as needed
 		float new_x = this.transform.position.x;
 		float new_y = this.transform.position.y;
 		float new_z = this.transform.position.z;
 
-		if(transform.position.x > x_goal){ 
+		if(transform.position.x > x_goal){//check if object has reached the x-goal 
 			new_x= new_x - x_rate;
 		}
 		else if(transform.position.x < x_goal){
 			new_x = new_x + x_rate;
 		}
 		
-		if(transform.position.y > y_goal){ 
+		if(transform.position.y > y_goal){ //check if object has reached the y-goal 
 			new_y= new_y - y_rate;
 		}
 		else if(transform.position.y < y_goal){
 			new_y = new_y + y_rate;
 		}
 		
-		if(transform.position.z > z_goal){ 
+		if(transform.position.z > z_goal){ //check if object has reached the z-goal 
 			new_z = new_z - z_rate;
 		}
 		else if(transform.position.z < z_goal){
 			new_z = new_z + z_rate;
 		}
 		
-		transform.position = new Vector3(new_x, new_y, new_z);
+		transform.position = new Vector3(new_x, new_y, new_z);//update the position
+		
+		float encounter = Random.value;//generate random number
+		if(encounter <= 0.5){
+			transform.position = new Vector3(0, 0, 0);
+		}
 	}
 }
