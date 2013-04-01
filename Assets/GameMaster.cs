38e6cbb4d59b98adedb4186592ab485264a8f0ee
William Wettersten <wbw20@case.edu>
2013-04-01 17:42:40 -0400

    progress on HUD

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -1,30 +1,41 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class GameMaster : MonoBehaviour {
-	private Rect windowRect = new Rect (Screen.width - 300, Screen.height - 600, Screen.width - 200, Screen.height - 150);
-	public Font spaceQuestFont;
-
-	// Use this for initialization
-	void Start () {
+namespace AssemblyCSharp {
+	public class GameMaster : MonoBehaviour {
+		private Rect windowRect = new Rect (Screen.width - 300, Screen.height - 600, Screen.width - 200, Screen.height - 150);
+		List<Planet> planets;
+		
+		public Font spaceQuestFont;
+		
 	
-	}
+		// Use this for initialization
+		void Start () {
+			planets = new List<Planet>();
+			planets.Add(new Planet("Jupiter"));
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+		
+		void OnGUI () {
+			windowRect = GUI.Window (0, windowRect, WindowFunction, "My Window");
+		}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	void OnGUI () {
-		windowRect = GUI.Window (0, windowRect, WindowFunction, "My Window");
-	}
-
-	void WindowFunction (int windowID) {
-		if (!spaceQuestFont) {
-            Debug.LogError("No font found, assign one in the inspector.");
-            return;
-        }
-		GUI.skin.font = spaceQuestFont;
-		GUI.Label (new Rect (10, 10, 50, 50), "Label");
+		void WindowFunction (int windowID) {
+			if (!spaceQuestFont) {
+	            Debug.LogError("No font found, assign one in the inspector.");
+	            return;
+	        }
+			GUI.skin.font = spaceQuestFont;
+			GUI.Label (new Rect (10, 10, 100, 50), "Space Quest");
+			
+			foreach (Planet planet in planets) {
+				GUI.Label (new Rect (10, 30, 100, 50), planet.getName());
+			}
+		}
 	}
 }
---
+++ Assets/Planet.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AssemblyCSharp
+{
+	public class Planet
+	{
+		private String name;
+		
+		public Planet (String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+}
+
